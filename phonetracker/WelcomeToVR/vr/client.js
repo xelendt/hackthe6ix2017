c7600f676a4d5133dfac69984f81b5f3ad8518c6
xelendt <m@xelendt.com>
2017-08-26 12:32:06 -0400

    three js does not import

--- phonetracker/WelcomeToVR/vr/client.js
+++ phonetracker/WelcomeToVR/vr/client.js
@@ -5,22 +5,36 @@
 // Auto-generated content.
 import {VRInstance} from 'react-vr-web';
 
+import {THREE} from "three";
 
 function init(bundle, parent, options) {
+  
+  //const scene = new THREE.Scene();
+
+  //console.log( vr );
   const vr = new VRInstance(bundle, 'WelcomeToVR', parent, {
     // Add custom options here
-    ...options,
+    cursorVisibility: 'visible',
   });
+
+  const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial()
+    );
+  cube.position.z = -1;
+  scene.add( cube );
+
   vr.render = function() {
     // Any custom behavior you want to perform on each frame goes here
     // console.log( vr );
     console.log( "HELLO FROM RENDER LOOP");
+    console.log( vr );
   };
 
-  // window.addEventListener("deviceorientation", function(event) {
-  //     // process event.alpha, event.beta and event.gamma
-  //     console.log( event.alpha );
-  // }, true);
+  window.addEventListener("deviceorientation", function(event) {
+      // process event.alpha, event.beta and event.gamma
+      console.log( event.alpha );
+  }, true);
 
   // Begin the animation loop
   vr.start();
