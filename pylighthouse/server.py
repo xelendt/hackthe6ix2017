55e1515bd842e2f906b92a0d365f6f6e3827019d
xelendt <m@xelendt.com>
2017-08-27 04:13:18 -0400

    opencv is for noobz, real programmers write from scratch every single time theymess up

--- pylighthouse/server.py
+++ pylighthouse/server.py
@@ -17,6 +17,13 @@ URL = "http://localhost:3001/stateById/59a20c47ea48642c64d7a425"
 
 svm = cv2.ml.SVM_load("svm_data.txt")
 
+def dist(loc_x, loc_y, a, b):
+	return (loc_x[a] - loc_x[b])*(loc_x[a] - loc_x[b]) + (loc_y[a] - loc_y[b])*(loc_y[a] - loc_y[b])
+
+def getAverage(loc_x, loc_y, a, b, c):
+	return ((loc_x[a]+loc_x[b]+loc_x[c])/3.0, (loc_y[a]+loc_y[b]+loc_y[c])/3.)	
+
+
 while(True):
 	print ("frame")
 	#capture frame
@@ -28,12 +35,10 @@ while(True):
 	DATA = { 'moving': False, 'x': 0, 'y': 0.123, 'z':0 }
 
 	# send the http request
-	r = requests.put(URL, json=DATA)
+	#r = requests.put(URL, json=DATA)
 
-	print( r.status_code )
-	print( r.reason )
-
-	break
+	#print( r.status_code )
+	#print( r.reason )
 
 	print( len(frame) ) 
 	print( len(frame[0]) )
@@ -79,13 +84,55 @@ while(True):
 
 	#print(z)
 
-	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-	ret,label,center=cv2.kmeans(z,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+	#criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+	#ret,label,center=cv2.kmeans(z,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 	#print(center[:,0])
 	#print(center[:,1])
 	#print(ret)
-	print(len(center[0]))
-	cv2.circle(frame, (int(center[0][0]*step), int(center[1][0]*step)), 50, (255, 0, 0), -1)
+	# print(len(center[0]))
+	# a = z[label.ravel()==0]
+	# b = z[label.ravel()==1]
+	# print(a)
+	# print(b)
+	# print(len(a), len(b))
+	# to_use = []
+	# if(len(a) > len(b)):
+	# 	to_use = a
+	# else:
+	# 	to_use = b
+
+	# calculate the center
+	a = 0
+	b = 0
+	if( len(loc_x) < 3 ):
+		continue
+
+	allowed = True
+	d1 = 1000
+	d2 = 2000
+	while(d1 > 5):
+		b += 1
+		if(b == len(loc_y)):
+			allowed = False
+			break
+		d1 = dist(loc_x, loc_y, a, b)
+
+	c = b
+	while(d2 > 5 and allowed):
+		c += 1
+		if(c == len(loc_y)):
+			allowed = False
+			break
+		d2 = dist(loc_x, loc_y, a, c)
+	#d3 = dist(loc_x, loc_y, a, c)
+
+	if( not allowed ):
+		continue
+
+	new_avg = getAverage(loc_x, loc_y, a, b, c)
+
+
+	cv2.circle(frame, (int(new_avg[0]*step), int(new_avg[1]*step)), 50, (255, 0, 0), -1)
 
 	cv2.imshow('frame', frame)
 
