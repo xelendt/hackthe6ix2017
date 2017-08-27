9cce39a4970e24e0a29db57842aae4971fb79b75
Andrew Au <iamandrewau@gmail.com>
2017-08-27 02:51:56 -0400

    fix IP

--- apiServer/server.js
+++ apiServer/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://10.129.21.180:8000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
