553fb45a9913297108035c2e953cc485af0d5996
xelendt <m@xelendt.com>
2017-08-27 00:17:55 -0400

    python server and training

--- pylighthouse/server.py
+++ pylighthouse/server.py
@@ -5,15 +5,57 @@ import requests
 
 cap = cv2.VideoCapture(0)
 
+cellWidth = 120
+cellHeight = 75
+svmSize = cellWidth*cellHeight*6
+
+#URL = "http://10.129.21.246:3001/stateById/59a1d31bf81c891c66871114"
+#URL = "http://42e31213.ngrok.io/stateById/59a1d31bf81c891c66871114"
+URL = "http://localhost:3001/stateById/59a20c47ea48642c64d7a425"
+
+svm = cv2.ml.SVM_load("svm_data.txt")
+
 while(True):
+	print ("frame")
 	#capture frame
 	ret, frame = cap.read()
 
 	# do the check on the frame
 
-	# send the http request
 
+	DATA = { 'moved': False }
+
+	# send the http request
+	r = requests.put(URL, json=DATA)
+
+	#print( r.status_code )
+	#print( r.reason )
+
+	cell = []
+	for i in range( 0, cellHeight ):
+		cell.append(frame[i][0:cellWidth])
+
+	cell = np.array(cell, dtype=np.float32)
+	#cell = frame[50:50, 100:100]
+	#print(frame[0][0])
+	#print(len(frame[50:50, 100:100]))
+	#cv2.imshow('frame', np.array(cell))
+
+	gx = cv2.Sobel( cell, cv2.CV_32F, 1, 0 )
+	gy = cv2.Sobel( cell, cv2.CV_32F, 0, 1 )
+
+	#cv2.imshow('frame', frame)
+	cv2.imshow('gx', gx)
+	cv2.imshow('gy', gy)
+	data = np.hstack((gx, gy))
+
+	cv2.imshow('data', data)
 	cv2.imshow('frame', frame)
+
+	#print(svmSize)
+
+	print(svm.predict(data.reshape(-1, svmSize))[1][0])
+	
 	if( cv2.waitKey(1) & 0xFF == ord('q') ):
 		break
 
---
+++ pylighthouse/train.py
@@ -0,0 +1,68 @@
+import numpy as np
+import cv2
+
+size = 20
+
+svm_params = dict( kernel_type = cv2.ml.SVM_LINEAR,
+                    svm_type = cv2.ml.SVM_C_SVC,
+                    C=2.67, gamma=5.383 )
+
+affine_flags = cv2.WARP_INVERSE_MAP|cv2.INTER_LINEAR
+
+# load in the data
+fgrad = open( "../lighthouse/grad_data.txt", "r" )
+fres = open( "../lighthouse/grad_results.txt", "r" )
+
+training_data = []
+training_results = []
+
+fname = fgrad.readline() 
+fname1 = fname
+while( len( fname ) > 0 ):
+	#print( ("Users/max/Desktop/hackthe6ix/lighthouse/"+fname)[:-1] )
+	img = cv2.imread(("../lighthouse/"+fname)[:-1]) 
+
+	training_data.append(img.flatten())
+	#print(len(img.flatten()))
+	fname = fgrad.readline()
+
+	#training_data.append(a)
+
+r = fres.readline()
+while( len(r) > 0 ):
+
+	training_results.append( int(r) )
+
+	r = fres.readline()
+
+print( training_results )
+
+# train
+# https://stackoverflow.com/questions/37038861/creating-svm-with-opencv-3-0-1-python-2-7
+svm = cv2.ml.SVM_create()
+svm.setType(cv2.ml.SVM_C_SVC)
+svm.setKernel(cv2.ml.SVM_LINEAR)
+svm.setTermCriteria((cv2.TERM_CRITERIA_COUNT, 10, 1.e-06))
+
+print( len(training_data) )
+print( len(training_data[0]))
+
+svm.train(np.array(training_data, dtype=np.float32), cv2.ml.ROW_SAMPLE, np.array(training_results))
+svm.save("svm_data.txt")
+
+print(fname1)
+test = cv2.imread("../lighthouse/train_grad/644449426.png")
+cv2.imshow('test', test)
+cv2.waitKey(0)
+
+test = test.reshape(-1, 54000);
+
+print(svm.predict(np.array(test, dtype=np.float32))[1])
+#print( svm.getSupportVectors() )
+
+print( "Done!") 
+
+# test
+
+
+# check accuracy
