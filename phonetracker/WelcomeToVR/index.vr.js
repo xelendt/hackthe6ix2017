660454fd6ee4836b7a1b3ae601ff200398e72d6c
xelendt <m@xelendt.com>
2017-08-29 11:43:34 -0400

    demo VR app

--- phonetracker/WelcomeToVR/index.vr.js
+++ phonetracker/WelcomeToVR/index.vr.js
@@ -7,9 +7,89 @@ import {
   View,
   VrButton,
 } from 'react-vr';
+import TimerMixin from 'react-timer-mixin';
+
+var g_x = 0;
+var g_y = 0;
+var g_z = 0;
 
 export default class WelcomeToVR extends React.Component {
+
+  // constructor()
+  // {
+  //   super();
+  //   console.log("init vr class");
+  //   // fetch('https://privo.serveo.net/stateById/59a20c47ea48642c64d7a425'//, {
+  //   //   //method: 'GET',
+  //   //   // headers: {
+  //   //   //   //'Accept': 'application/json',
+  //   //   // },
+  //   // ).then((response) => response.json())
+  //   //   .then((responseJson) => {
+  //   //     console.log(responseJson);
+  //   //     this.setState({x:0, y:0, z:-2});
+  //   //     g_x = responseJson.x;
+  //   //     g_y = responseJson.y;
+  //   //     g_z = responseJson.z;
+  //   //   });
+  //     this.xp = 0;
+  //     setInterval(this.render, 2000);
+  // }
+
+  // ComponentWillMount(){
+  //   p = fetch('https://privo.serveo.net/stateById/59a20c47ea48642c64d7a425'//, {
+  //   //   //method: 'GET',
+  //   //   // headers: {
+  //   //   //   //'Accept': 'application/json',
+  //   //   // },
+  //    ).then((response) => response.json())
+  //      .then((responseJson) => {
+  //        console.log(responseJson);
+  //        if(responseJson == undefined)
+  //        {
+  //        console.log( "UNDEFINED RESPONSE ??");
+  //        }
+  //        g_x = responseJson.x;
+  //        g_y = responseJson.y;
+  //        g_z = responseJson.z;
+        
+  //        console.log("finished after the rest");
+  //      });
+  // }
+
   render() {
+
+    //g_x += 1;
+    // g_y += 1;
+    // g_z += 1;
+    // console.log( g_x );
+    //xp = -1;
+    //this.x = 0;
+    //this.y = 0;
+    //this.z = 0;
+    // p = fetch('https://privo.serveo.net/stateById/59a20c47ea48642c64d7a425'//, {
+    //   //method: 'GET',
+    //   // headers: {
+    //   //   //'Accept': 'application/json',
+    //   // },
+    // ).then((response) => response.json())
+    //   .then((responseJson) => {
+    //     console.log(responseJson);
+    //     if(responseJson == undefined)
+    //     {
+    //       console.log( "UNDEFINED RESPONSE ??");
+    //     }
+    //     g_x = responseJson.x;
+    //     g_y = responseJson.y;
+    //     g_z = responseJson.z;
+        
+    //     console.log("finished after the rest");
+    //   });
+
+    //console.log(this.xp);
+
+    //Promise.all([p]).then({});
+
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
@@ -27,9 +107,9 @@ export default class WelcomeToVR extends React.Component {
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            transform: [{translate: [0, 0, -2]}],
           }}>
-          hello2
+          hi
         </Text>
         </VrButton>
       </View>
--- phonetracker/WelcomeToVR/vr/client.js
+++ phonetracker/WelcomeToVR/vr/client.js
@@ -11,7 +11,7 @@ accel = null;
 vel = {x:0,
        y:0,
        z:0};
-pos = {x:0,
+var pos = {x:0,
        y:0,
        z:0};
 
@@ -21,9 +21,17 @@ drift = {x: 0,
          y: 0,
          z: 0};
 
+initial_size = 4;
+
 thresh = 1; // threshold for acceleration
 
-damper = 13; // damper for acceleration
+damper = 1; // damper for acceleration
+
+var sensor_allowance = 0.0;
+
+var prevPos = {'x':0,
+               'y':0,
+               'z':0};
 
 
 const cube = new THREE.Mesh(
@@ -47,6 +55,7 @@ function init(bundle, parent, options) {
 
 
 
+
   cube.position.z = -1;
   //cube.material.color = {1, 0, 0};
   scene.add( cube );
@@ -56,7 +65,7 @@ function init(bundle, parent, options) {
 
   vr.render = function() {
     // Any custom behavior you want to perform on each frame goes here
-    // console.log( vr );
+     //console.log( vr );
 
     var request = new XMLHttpRequest();
     request.onreadystatechange = (e) => {
@@ -65,15 +74,42 @@ function init(bundle, parent, options) {
       }
 
       if (request.status === 200) {
+
+        console.log( prevPos, pos )
+        if( prevPos.x != pos.x || prevPos.y != pos.y || prevPos.z || pos.z )
+        {
+          prevPos = pos;
+        }
+
         api_data = JSON.parse(request.responseText);
+        sensor_allowance = 1.0;
 
       } else {
-        console.warn('error');
+        //console.log(request);
+        
       }
     };
 
-    request.open('GET', 'https://privo.serveo.net/stateById/59a20c47ea48642c64d7a425');
-    request.send();
+text = "fu";
+
+
+// request.open('POST', 'https://impedio.serveo.net/state');
+
+// request.setRequestHeader("Content-type", "application/json");
+// request.setRequestHeader("X-Requested-With", "origin");
+
+//request.setRequestHeader("Content-length", 2);
+//request.setRequestHeader("Connection", "close");        
+
+
+        // request.send({'moving':false,
+        //               'x':0,
+        //               'y':0,
+        //               'z':0,
+        //               'error':'text'});
+
+     request.open('GET', 'https://privo.serveo.net/stateById/59a20c47ea48642c64d7a425');
+     request.send();
 
     // https://www.html5rocks.com/en/tutorials/cors/
     // var xhr = createCORSRequest('GET', 'https://privo.serveo.net/stateById/59a20c47ea48642c64d7a425');
@@ -92,6 +128,7 @@ function init(bundle, parent, options) {
 
     // xhr.withCredentials = true;
     // xhr.send();
+    //console.log( pos, prevPos );
 
     cube.rotation.y += 0.1;
     //console.log( "HELLO FROM RENDER LOOP");
@@ -103,7 +140,7 @@ function init(bundle, parent, options) {
     {
       cube.position.y = accel.x/10;
     }
-    if(accel)
+    if(accel && sensor_allowance > 0)
     {
       cube.material.color.r = 0;
       //console.log( accel );
@@ -117,17 +154,38 @@ function init(bundle, parent, options) {
       if( Math.abs(accel.z) > thresh ){
         vel.z += (accel.z )/ damper;
       }
-      
     }
-    pos.x += vel.x;
-    pos.y += vel.y;
-    pos.z += vel.z;
+
+    console.log( sensor_allowance );
+    if( sensor_allowance > 0 )
+    {
+      console.log( vel );
+      pos.x += (vel.x * 250000000) + Math.sign(vel.x)*25;
+      pos.y += (vel.y * 250000000) + Math.sign(vel.x)*40;
+      pos.z += (vel.z * 250000000) + Math.sign(vel.x)*25;
+      sensor_allowance -= 0.05;
+    }
+    else
+    {
+      //vel.x = 0;
+      //vel.y = 0;
+      //vel.z = 0;
+    }
 
     if( api_data )
     {
-      console.log( "WE GOT DATA" );
-      pos.x = api_data.x;
-      pos.y = api_data.y;
+      //console.log( "WE GOT DATA" );
+      pos.x = api_data.x-640;
+      pos.y = api_data.y-360;
+      if( initial_size < 0 )
+      {
+        initial_size = api_data.z;
+      }
+      size = api_data.z;
+      pos.z = 50 * ( initial_size - size );
+      vel.x = prevPos.x - pos.x;
+      vel.y = prevPos.y - pos.y;
+      vel.z = prevPos.z - pos.z;
     }
 
     vr.player.camera.position.x = pos.x;
